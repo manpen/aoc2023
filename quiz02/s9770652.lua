932ea698c3764e46450181bd30f144a75fb6c94f
s9770652 <s9770652@stud.uni-frankfurt.de>
2023-12-02 11:17:13 +0100

    Lösung für Tag 2.
    Signed-off-by: s9770652 <s9770652@stud.uni-frankfurt.de>

---
+++ quiz02/s9770652.lua
@@ -0,0 +1,20 @@
+local bag = { red = 12, green = 13, blue = 14 }
+local ids = 0
+local powers = 0
+for line in io.lines("s9770652.txt") do
+    local possible = true
+    local max = { red = 0, green = 0, blue = 0 }
+    for colour, _ in pairs(bag) do
+        for number in line:gmatch("(%d*) " .. colour) do
+            possible = possible and tonumber(number) <= bag[colour]
+            max[colour] = math.max(max[colour], tonumber(number))
+        end
+    end
+    if possible then
+        ids = ids + line:match("Game (%d*)")
+    end
+    powers = powers + max.red * max.green * max.blue
+end
+
+print(("Die Summe der IDs betr„gt %d."):format(ids))
+print(("Die Summe der M„chtigkeiten betr„gt %d."):format(powers))
