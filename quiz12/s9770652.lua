3c40c99888739c5aa7698f4525e3422d5ac513f4
s9770652 <s9770652@stud.uni-frankfurt.de>
2023-12-17 08:55:52 +0100

    Lösung für Tag 12.
    Signed-off-by: s9770652 <s9770652@stud.uni-frankfurt.de>

---
+++ quiz12/s9770652.lua
@@ -0,0 +1,67 @@
+dofile "..\\quiz01\\s9770652_commons.lua"
+
+---@class Cache
+local Cache = {}
+
+---@type fun(self: Cache, obj?: table) : Cache
+function Cache:new(obj)
+    obj = obj or {}
+    setmetatable(obj, self)
+    self.__index = self
+    return obj
+end
+
+---@type fun(self: Cache, tocache: number, i: number, j: number, k: number)
+function Cache:cache(tocache, i, j, k)
+    if not self[i] then self[i] = {} end
+    if not self[i][j] then self[i][j] = {} end
+    self[i][j][k] = tocache
+end
+
+---@type fun(self: Cache, i: number, j: number, k: number) : number|nil
+function Cache:getCached(i, j, k)
+    return self[i] and self[i][j] and self[i][j][k]
+end
+
+-- Visits each spring in `springs`.
+---@param springs table All springs as array.
+---@param dmgd table Array of sizes of all blocks of damaged springs.
+---@param si number Index of current spring.
+---@param di number Index of current damaged block.
+---@param dLength number Length of traversed part of current damaged block.
+---@param cache Cache 
+function GetArrangementCount(springs, dmgd, si, di, dLength, cache)
+    local cached = cache:getCached(si, di, dLength)
+    if cached then return cached end
+    if si == #springs then  -- Due to padding, the last spring is always ".".
+        return (di == #dmgd and dLength == 0 and 1) or 0  -- Check if all damaged blocks have been closed.
+    end
+    local ac = 0
+    if springs[si+1] == "." or springs[si+1] == "?" then
+        if dLength == 0 then  -- No damaged block to close; previous spring "." or "?".
+            ac = ac + GetArrangementCount(springs, dmgd, si+1, di, 0, cache)
+        elseif dLength > 0 and di < #dmgd and dmgd[di+1] == dLength then  -- Close damaged block.
+            ac = ac + GetArrangementCount(springs, dmgd, si+1, di+1, 0, cache)
+        end
+    end
+    if springs[si+1] == "#" or springs[si+1] == "?" then  -- Continue current damaged block.
+        ac = ac + GetArrangementCount(springs, dmgd, si+1, di, dLength+1, cache)
+    end
+    cache:cache(ac, si, di, dLength)
+    return ac
+end
+
+local lines = ReadAllLines()
+for _, reps in ipairs({1, 5}) do
+    local springs, dmgd = {}, {}
+    for k, line in ipairs(lines) do
+        local l = line:split()
+        springs[k] = (l[1]:rep(reps, "?").."."):totable(".")  -- Add final "." as padding.
+        dmgd[k] = (l[2]:rep(reps, ",").."."):totable("%d+", tonumber)
+    end
+    local sum = 0
+    for i = 1, #springs do
+        sum = sum + GetArrangementCount(springs[i], dmgd[i], 0, 0, 0, Cache:new())
+    end
+    print(("Mit %d-facher Faltung betraegt die Anzahl an moeglichen Kombination %d."):format(reps, sum))  -- solution of both parts
+end
